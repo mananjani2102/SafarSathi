2dd7fb0ad7eabfad70e330ca6a5d4a5d8484106f
Manan Jani <manan.jani.cg@gmail.com>
2026-02-26 10:00:00 +0530

    feat: add auth controller with login and register

---
+++ backend/controllers/authController.js
@@ -0,0 +1,90 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
+};
+
+const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please provide name, email, and password");
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400);
+      throw new Error("An account with this email already exists");
+    }
+
+    const user = await User.create({ name, email, password });
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Please provide email and password");
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMe = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      success: true,
+      data: req.user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login, getMe };
