e39667ab17a4bd11ad112f81641f7819efbe601e
Manan Jani <manan.jani.cg@gmail.com>
2026-03-01 12:20:00 +0530

    feat: bootstrap SafarSathi frontend app

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider } from "./context/ThemeContext";
+import store from "./redux/store";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+          <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
